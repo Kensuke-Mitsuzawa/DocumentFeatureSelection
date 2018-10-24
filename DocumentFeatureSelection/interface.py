e9a4276b31daef833c21c6d3e5b2ff3a66c17ebd
Kensuke-Mitsuzawa <kensuke.mit@gmail.com>
2018-10-24 16:40:40 +0900

    clean up code following pep

--- DocumentFeatureSelection/interface.py
+++ DocumentFeatureSelection/interface.py
@@ -14,38 +14,40 @@ METHOD_NAMES = ['soa', 'pmi', 'tf_idf', 'bns']
 N_FEATURE_SWITCH_STRATEGY = 1000000
 
 
-def decide_joblib_strategy(feature2id_dict:Dict[str,int])->str:
+def decide_joblib_strategy(feature2id_dict: Dict[str, int])->str:
     if len(feature2id_dict) > N_FEATURE_SWITCH_STRATEGY:
         return 'threading'
     else:
         return 'multiprocessing'
 
 
-def run_feature_selection(input_dict:AvailableInputTypes,
-                          method:str,
-                          use_cython:bool=False,
-                          is_use_cache:bool=False,
-                          is_use_memmap:bool=False,
-                          cache_backend='PersistentDict',
-                          path_working_dir:str=None,
+def run_feature_selection(input_dict: AvailableInputTypes,
+                          method: str,
+                          use_cython: bool=False,
+                          is_use_cache: bool=False,
+                          is_use_memmap: bool=False,
+                          cache_backend: str='PersistentDict',
+                          path_working_dir: str=None,
                           matrix_form=None,
-                          n_jobs:int=1)->ScoredResultObject:
+                          n_jobs: int=1)->ScoredResultObject:
     """A interface function of DocumentFeatureSelection package.
 
     * Args
     - input_dict: Dict-object which has category-name as key and list of features as value.
         - You can put dict or sqlitedict.SqliteDict, or DocumentFeatureSelection.models.PersistentDict
     - method: A method name of feature selection metric
-    - use_cython: boolean flag to use cython code for computation. It's much faster to use cython than native-python code
+    - use_cython: boolean flag to use cython code for computation.
+    It's much faster to use cython than native-python code
     - is_use_cache: boolean flag to use disk-drive for keeping objects which tends to be huge.
     - is_use_memmap: boolean flag to use memmap for keeping matrix object.
     - path_working_dir: str object.
-        - The file path to directory where you save cache file or memmap matrix object. If you leave it None, it finds some directory and save files in it.
+        - The file path to directory where you save cache file or memmap matrix object. If you leave it None,
+        it finds some directory and save files in it.
     - cache_backend
         - Named of cache backend if you put True on is_use_cache. [PersistentDict, SqliteDict]
 
     """
-    if not method in METHOD_NAMES:
+    if method not in METHOD_NAMES:
         raise Exception('method name must be either of {}. Yours: {}'.format(METHOD_NAMES, method))
 
     if method == 'tf_idf':
@@ -119,7 +121,8 @@ def run_feature_selection(input_dict:AvailableInputTypes,
         """You get scored-matrix with bns.
         ATTENTION: #label should be 2 always.
         """
-        ### Consider shorter label name as positive label(positive and negative does NOT have any meaning in this context) ###
+        # Consider shorter label name as positive label
+        # (positive and negative does NOT have any meaning in this context) #
         positive_label_name = sorted(input_dict.keys(), key=lambda x: len(x))[0]
 
         if len(input_dict.keys()) >= 3:
